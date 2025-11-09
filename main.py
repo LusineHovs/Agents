01ca310f2861bda641a68fe13b856198bb950183
LusineHovsepyan <lusine.a.hovsepyan@gmail.com>
2025-11-09 11:33:33 +0400

    Simple agent, which uses localy defined tool to extract the number of characters of the given text.

---
+++ main.py
@@ -0,0 +1,67 @@
+from typing import List
+from typing import Union
+from dotenv import load_dotenv
+from langchain_core.tools import render_text_description, tool
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
+# from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
+import langchain
+from ollama import Tool
+
+load_dotenv()
+
+@tool
+def get_text_length(text: str) -> int:
+    """Get the length of a text"""
+    return len(text)
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")
+
+if __name__ == "__main__":
+    print("Hello World")
+    tools = [get_text_length]
+
+    template = """Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:
+"""
+# prompt = PromptTemplate(template=template, input_variables=["input", "tools", "tool_names"])
+prompt = PromptTemplate.from_template(template).partial(
+    tools=render_text_description(tools),
+    tool_names=", ".join([t.name for t in tools]))
+
+llm = ChatOllama(model="llama3.2", temperature=0, model_kwargs={"stop":["\nObservation:"]})
+agent = {"input": lambda x: x["input"]} | prompt | llm | StrOutputParser()
+
+agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of the string DOG?"})
+print(agent_step)
+
+if isinstance(agent_step, AgentAction):
+    tool_name = agent_step.tool
+    tool_to_use = find_tool_by_name(tools,tool_name)
+    tool_input = agent_step.tool_input
+
+    observation = tool_to_use.func(str(tool_input))
+    print(f"{observation=}")
